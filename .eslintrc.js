c82e04a4b9645533a7f60ea49a1a85ecc368d599
Roberto Monteiro <fye@live.dk>
2019-10-23 15:33:19 -0300

    [ initial ] ambiente montado com ligação para deploy em Firebase

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  'extends': [
+    'plugin:vue/essential',
+    'eslint:recommended',
+    'vuetify'
+  ],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'prefer-promise-reject-errors': 'off',
+    'comma-dangle': [ 'error', 'never' ]
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  }
+}
---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app>
+    <v-content>
+      <hello-world />
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+  import HelloWorld from './components/HelloWorld'
+
+  export default {
+    name: 'App',
+    components: {
+      HelloWorld
+    },
+    data: () => ({
+      //
+    })
+  }
+</script>
