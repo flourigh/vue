a736bfd23780e858a369609e846fccd7fb92f0b5
Roberto Monteiro <fye@live.dk>
2019-12-18 09:13:53 -0300

    [ layout ] dialogs para loading e card

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,48 +2,76 @@
   <v-container
     class="px-0 mx-0 mx-auto max-width-412"
   >
-    <v-card
+    <div
       v-if="loading"
-      class="text-center transparent"
+      class="text-center"
     >
-      <v-progress-circular
-        :size="70"
-        :width="7"
-        color="primary"
-        indeterminate
-      />
-    </v-card>
-
-    <v-card
-      v-else
-      class="pa-4 transparent"
-    >
-      <v-card-text>
-        <v-text-field
-          v-for="(keys, i) in uri"
-          :id="`password-${i}`"
-          :key="`password-${i}`"
-          v-model="uri[i]"
+      <v-dialog
+        v-model="loading"
+        persistent
+        width="300"
+      >
+        <v-card
           color="primary"
-          readonly
-          append-outer-icon="mdi-content-copy"
-          @click:append-outer="copy(`password-${i}`)"
-        />
-      </v-card-text>
-
-      <v-card-actions>
-        <v-btn
-          class="mx-auto"
-          width="256"
-          height="56"
-          color="primary"
-          large
-          @click="getPassword()"
+          dark
         >
-          Nova senha
-        </v-btn>
-      </v-card-actions>
-    </v-card>
+          <v-card-text
+            class="py-5"
+          >
+            <v-progress-linear
+              indeterminate
+              color="white"
+              class="mb-0"
+            />
+          </v-card-text>
+        </v-card>
+      </v-dialog>
+    </div>
+
+    <div
+      v-else
+      class="text-center"
+    >
+      <v-dialog
+        v-model="uri"
+        persistent
+        content-class="max-width-412 min-height-320"
+      >
+        <v-card
+          class="pa-4 transparent"
+          flat
+        >
+          <v-card-text>
+            <v-text-field
+              v-for="(keys, i) in uri"
+              :id="`password-${i}`"
+              :key="`password-${i}`"
+              v-model="uri[i]"
+              color="primary"
+              readonly
+              append-outer-icon="mdi-content-copy"
+              @click:append-outer="copy(`password-${i}`)"
+            />
+          </v-card-text>
+
+          <v-card-actions>
+            <v-btn
+              color="primary"
+              dark
+              absolute
+              bottom
+              right
+              fab
+              @click="getPassword()"
+            >
+              <v-icon>
+                mdi-cached
+              </v-icon>
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </div>
   </v-container>
 </template>
 
